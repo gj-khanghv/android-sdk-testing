baa78b66ca6830ed23f8aba07e08ca4fce8d4687
Khang Huynh Vy <khang.huynh@bstarsolutions.com>
2024-04-26 09:28:45 +0700

    Done implement api profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("com.github.gj-khanghv:android-sdk-testing:release-04")
+//    implementation("com.github.gj-khanghv:android-sdk-testing:release-04")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
