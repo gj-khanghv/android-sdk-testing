24fe7f334b47ba2d83e0a743435d253e1c432e9d
Khang Huynh Vy <khang.huynh@bstarsolutions.com>
2024-05-03 15:21:14 +0700

    v4

--- android-sdk-testing/src/main/java/loy/mobile/android_sdk_testing/AndroidSDK.kt
+++ android-sdk-testing/src/main/java/loy/mobile/android_sdk_testing/AndroidSDK.kt
@@ -2,7 +2,7 @@ package loy.mobile.android_sdk_testing
 
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import androidx.activity.result.ActivityResultLauncher
 import loy.mobile.android_sdk_testing.activity.AuthActivity
 import loy.mobile.android_sdk_testing.model.UserModel
@@ -21,7 +21,7 @@ class AndroidSDK {
      */
     fun signIn(context: Context?, launcher: ActivityResultLauncher<Intent>) {
         val intent = Intent(context, AuthActivity::class.java).apply {
-            setFlags(FLAG_ACTIVITY_SINGLE_TOP)
+            setFlags(FLAG_ACTIVITY_CLEAR_TOP)
             putExtra("method", "signIn")
         }
         launcher.launch(intent)
@@ -34,7 +34,7 @@ class AndroidSDK {
      */
     fun signUp(context: Context?, launcher: ActivityResultLauncher<Intent>) {
         val intent = Intent(context, AuthActivity::class.java).apply {
-            setFlags(FLAG_ACTIVITY_SINGLE_TOP)
+            setFlags(FLAG_ACTIVITY_CLEAR_TOP)
             putExtra("method", "signUp")
         }
         launcher.launch(intent)
